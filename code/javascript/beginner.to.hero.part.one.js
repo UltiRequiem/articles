646998cf91ec18c0e6db937ba96e1e2b31bd8453
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-20 14:43:07 -0500

    :sparkles: Update

---
+++ code/javascript/beginner.to.hero.part.one.js
@@ -0,0 +1,12 @@
+/**
+ * @param numberOne {number} The first number to sum
+ * @param numberTwo {number} The second number to sum
+ */
+function sum(numberOne, numberTwo) {
+  const numberOnePlusNumberTwo = numberOne + numberTwo;
+  return numberOnePlusNumberTwo;
+}
+
+const myFavoriteNumber = sum(3, 4);
+
+console.log(myFavoriteNumber);
